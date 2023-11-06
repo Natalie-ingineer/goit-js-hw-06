b570792c7224ff6aa80831e4a815346581626e58
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-06 20:06:36 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,44 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// Створи функцію createBoxes(amount), яка приймає один
+// параметр - число.Функція створює стільки < div >,
+// скільки вказано в amount і  додає їх у div#boxes.
+
+// 1 Розміри найпершого <div> - 30px на 30px.
+
+// 2 Кожен елемент після першого повинен бути ширшим
+// і вищим від попереднього на 10px.
+
+// 3 Всі елементи повинні мати випадковий колір фону
+// у форматі HEX.Використовуй готову функцію getRandomHexColor
+// для отримання кольору.
+
+// const textInput = document.querySelector("input");
+// console.log(textInput);
+// textInput.classList.add("text-input");
+// console.log(textInput.classList);
+
+// const btnCreate = document.querySelector("button[data-create]");
+// console.log(btnCreate);
+
+// const btnDestroy = document.querySelector("button[data-destroy]");
+// console.log(btnDestroy);
+
+// const div = document.querySelector("#boxes");
+// console.log(div);
+
+// btnCreate.addEventListener("click", createBoxes);
+// btnDestroy.addEventListener("click", destroyBoxes);
+
+// textInput.addEventListener("input", handleInput);
+
+// const amount = textInput.currentTarget.value;
+
+// function createBoxes(amount) {
+// let width = 30;
+// let height = 30;
+// }
+
+// function getRandomHexColor() {
+// return `#${Math.floor(Math.random() * 16777215)
+// .toString(16)
+// .padStart(6, 0)}`;
+// }
