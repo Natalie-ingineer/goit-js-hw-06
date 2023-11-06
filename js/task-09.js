562309be014fd0e64f9bfd2e422ca248dbbec905
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-06 12:08:48 +0200

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,22 @@
+const btn = document.querySelector(".change-color");
+console.log(btn);
+
+const body = document.querySelector("body");
+console.log(body);
+
+const colorSpan = document.querySelector(".color");
+console.log(colorSpan);
+
+btn.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const randomColor = getRandomHexColor();
+
+body.style.backgroundColor = randomColor;
+
+colorSpan.textContent = randomColor;
